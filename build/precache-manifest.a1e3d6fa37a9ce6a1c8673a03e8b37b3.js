5797ccabfa760b514dc073c946bc5fb834755fa8
Hien Nguyen <hiennguyensuika@gmail.com>
2023-10-27 23:47:09 +0700

    Movie App

---
+++ build/precache-manifest.a1e3d6fa37a9ce6a1c8673a03e8b37b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f686b17f41001a3718ab4a5e2b256440",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ce23f8461ac722d999b",
+    "url": "/static/css/main.e924726c.chunk.css"
+  },
+  {
+    "revision": "0daa7882a90b93846b1c",
+    "url": "/static/js/2.7d6ace1c.chunk.js"
+  },
+  {
+    "revision": "5ce23f8461ac722d999b",
+    "url": "/static/js/main.69a2e78c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
